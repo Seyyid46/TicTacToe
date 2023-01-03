637c589ccb3e0f33beca879f5de2a60f775f2ccf
syildiz <syildiz@techfak.de>
2023-01-03 20:22:17 +0100

    Create Method printGameBoard/Add Scanner in Main to get User Input

--- ttt.java
+++ ttt.java
@@ -7,7 +7,7 @@ This Programm is a TicTacToe Game on your Terminal
  
  public class Main{
 
- public static void main(String[] args) {
+    public static void main(String[] args) {
 
         //generating GameBoard in 2D Array
         char [][] gameboard = {{' ', '|', ' ', '|', ' '},
@@ -18,5 +18,22 @@ This Programm is a TicTacToe Game on your Terminal
             };
 
         printGameBoard(gameboard);//first Print
+
+        //generate Input
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please enter your Position (1-9)");
     }
+
+    /*
+    Method to print the Board
+     */
+    public static void printGameBoard(char[][] gameboard){
+        for (char[] row:gameboard){
+            for(char c : row){
+                System.out.print(c);
+            }
+            System.out.println();
+        }
+    }
+
 }
